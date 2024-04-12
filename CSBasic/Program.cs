a6349897582efd3c3dd6fc10137f5dd9f5381c5d
haeunNoh06 <okhagrace06@naver.com>
2024-04-12 10:10:11 +0900

    #7 int, double 자료형 최소, 최대값 출력 지수표기법에 주의

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -23,6 +23,12 @@ namespace CSBasic
             Console.WriteLine((long)a + b); // b의 자료형인 int가 long과 더해져 암묵적으로 long타입으로 변환이 되어 오버플로우가 일어나지 않는다. 
             Console.WriteLine(a + (long)b);
             Console.WriteLine((long)a + (long)b);
+
+            // 자료형 최소, 최대 값 출력하기
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
         }
     }
 }
