52b1b2d0ee0baf098df1c520ca2718c1631cb088
haeunNoh06 <okhagrace06@naver.com>
2024-04-12 09:54:57 +0900

    #6 오버플로우 해결

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -12,6 +12,17 @@ namespace CSBasic
         {
             // c#에서 헬로월드를 찍어보자 
             Console.WriteLine("Hello World?!");
+
+            // 오버플로우 연습
+            //int a = 2147483647;
+            int a = 2000000000; // 20억
+            int b = 1000000000; // 10억
+            Console.WriteLine(a+b);
+
+            // 오버플로우 연습2
+            Console.WriteLine((long)a + b); // b의 자료형인 int가 long과 더해져 암묵적으로 long타입으로 변환이 되어 오버플로우가 일어나지 않는다. 
+            Console.WriteLine(a + (long)b);
+            Console.WriteLine((long)a + (long)b);
         }
     }
 }
